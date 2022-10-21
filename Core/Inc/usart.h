076e52cf38183559e188d1b4945065a1b332456b
Petar Susac <petar.susac@fer.hr>
2022-10-21 13:29:17 +0200

    Increase UART buffer size

--- Core/Inc/usart.h
+++ Core/Inc/usart.h
@@ -35,7 +35,7 @@ extern "C" {
 extern UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN Private defines */
-#define BUFSIZE 512
+#define BUFSIZE 1024
 /* USER CODE END Private defines */
 
 void MX_USART1_UART_Init(void);
